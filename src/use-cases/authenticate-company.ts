f9b63479de6a59c1a2a036d49e1807b299268bdf
Pedro Requiao <alonsofts@gmail.com>
2023-12-27 00:07:31 -0500

    add: more tests for register-company

--- src/use-cases/authenticate-company.ts
+++ src/use-cases/authenticate-company.ts
@@ -22,7 +22,6 @@ export class AuthenticateCompanyUseCase {
     }: AuthenticateCompanyUseCaseRequest): Promise<AuthenticateCompanyUseCaseResponse> {
         const company = await this.companyRepository.findByEmail(email)
 
-        console.log('achei: ' + JSON.stringify(company))
         if (!company) {
             throw new InvalidCredentialsError()
         }
